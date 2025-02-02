1c614cd85f70c1656bc777b0d1704d29214ac782
abhmidane <abenhmidane@ueat.io>
2025-02-02 18:25:07 +0100

    seo optimazation

--- js/schema.js
+++ js/schema.js
@@ -1,1 +1,21 @@
- 
+const blogSchema = {
+  "@context": "https://schema.org",
+  "@type": "Blog",
+  name: "Test Automation & Quality Engineering Blog",
+  description:
+    "Expert insights on Test Automation, DevOps, and Software Quality Engineering",
+  url: "https://anouarbenhm.dev",
+  author: {
+    "@type": "Person",
+    name: "Anouar Ben Hmidane",
+    jobTitle: "Test Automation Leader & Quality Engineering Entrepreneur",
+    url: "https://anouarbenhm.dev/about",
+  },
+};
+
+document.querySelector("head").insertAdjacentHTML(
+  "beforeend",
+  `<script type="application/ld+json">
+    ${JSON.stringify(blogSchema)}
+  </script>`
+);
